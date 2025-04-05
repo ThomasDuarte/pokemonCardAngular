091903ff43051398eeb6c5e85ca4c609e471ef96
Thomas Duarte <thomas.duarte@supdevinci-edu.fr>
2025-04-05 02:32:03 +0200

    Create input signal for monster
    Learning how to use input signals on Angular

--- playingCard/src/app/components/playing-card/playing-card.component.ts
+++ playingCard/src/app/components/playing-card/playing-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, Input, InputSignal } from '@angular/core';
 import { Monster } from '../../models/monster.model';
 
 @Component({
@@ -8,13 +8,13 @@ import { Monster } from '../../models/monster.model';
   styleUrl: './playing-card.component.css',
 })
 export class PlayingCardComponent {
-  @Input({
-    alias: 'my-monster',
-    // Permet de modifier la value de l'input
-    // transform: (value: Monster) => {
-    //   value.hp = value.hp / 2;
-    //   return value;
-    // },
-  })
-  monster: Monster = new Monster();
+  /* @Input({
+  //   alias: 'my-monster',
+  //   // Permet de modifier la value de l'input
+  //   // transform: (value: Monster) => {
+  //   //   value.hp = value.hp / 2;
+  //   //   return value;
+  //   // },
+   })*/
+  monster: InputSignal<Monster> = input(new Monster());
 }
