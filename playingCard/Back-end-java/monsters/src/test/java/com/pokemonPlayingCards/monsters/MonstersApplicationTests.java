aafe0ebaf78a61259b69407675c136118919b755
Thomas Duarte <thomas.duarte@supdevinci-edu.fr>
2025-05-20 00:30:06 +0200

    Initialise the Spring boot API

---
+++ playingCard/Back-end-java/monsters/src/test/java/com/pokemonPlayingCards/monsters/MonstersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemonPlayingCards.monsters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonstersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
